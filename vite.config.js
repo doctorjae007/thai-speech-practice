f752ca1d294c90e9456627a8565303b840179181
doctorjae007 <89516364+doctorjae007@users.noreply.github.com>
2025-08-05 10:37:33 +0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/thai-speech-practice/',
+  plugins: [react(),tailwindcss()],
+})
